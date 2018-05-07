5459e6c9ca59a233428aa244e8c8d1f6dc63f6ff
Prionsais O'Lionnain <l00119705@student.lyit.ie>
2018-05-07 22:54:04 +0100

    Added Hello Lyit.java

---
+++ helloLyit.java
@@ -0,0 +1,6 @@
+public class HelloLYIT {
+    public static void main(String[] args) {
+        // Prints "Hello, LYIT from Prionsais" as output
+        System.out.println("Hello, LYIT from Prionsais");
+    }
+}
